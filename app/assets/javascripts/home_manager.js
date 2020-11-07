2d0f8b4691483f5a76aef51f5e3e3e5a9b6bda06
Pae <songju19990426@gmail.com>
2020-11-07 17:54:11 +0900

    20日以降のボタン・テキスト表示変更処理(店長)

--- app/assets/javascripts/home_manager.js
+++ app/assets/javascripts/home_manager.js
@@ -8,7 +8,12 @@ $(function(){
     let date = new Date();
     let today = parseInt(date.getDate(), 10);
     if(today < 20) {
-        $('#not_edit h2').text('20日以降に編集可能です');
+        $('#not_edit h2').show();
+        $('.confirm').hide();
+    }
+    else {
+        $('#not_edit h2').hide();
+        $('.confirm').show();
     }
     
     $('.edit-dropdown').on('click', function () {
